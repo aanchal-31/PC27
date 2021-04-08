fa1e90709c3eabfe991cb38214633db5db2fb20d
aanchal-31 <73495899+aanchal-31@users.noreply.github.com>
2021-04-08 18:51:42 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,22 @@
+class Slingshot{
+    constructor(bodyA,bodyB)
+    {
+        var options={
+        bodyA:bodyA,
+        bodyB:bodyB,
+        length:10,
+        stiffness:0.04
+
+        }
+        this.Slingshot=Constraint.create(options);
+        World.add(world,this.Slingshot);
+    }
+    display()
+    {
+var pointA =this.Slingshot.bodyA.position;
+var pointB =this.Slingshot.bodyB.position;
+strokeWeight(4);
+line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
+
